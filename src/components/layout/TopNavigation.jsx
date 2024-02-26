e8b5c269803ec7712bc51acb7e8640e38deaf7e8
Toni Inot <tonigwapo@gmail.com>
2024-02-26 21:46:08 +0800

    topnav text color

--- src/components/layout/TopNavigation.jsx
+++ src/components/layout/TopNavigation.jsx
@@ -35,14 +35,14 @@ const TopNavigation = () => {
 
     return(
         <>
-            <nav className="border-gray-200 bg-[#0f0E17]">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <nav className="border-gray-200 bg-[#0f0E17] ">
+                <div className="max-w-screen-xl flex text-[#fffffe]  flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span
-                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PFASt</span>
+                            className="self-center text-2xl font-semibold whitespace-nowrap">PFASt</span>
                     </a>
                     <button data-collapse-toggle="navbar-default" type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                             aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -57,14 +57,14 @@ const TopNavigation = () => {
                             {isParkingOwner ?
                                 <li>
                                     <Link to={'/myparking'}
-                                          className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
+                                          className="block py-2 px-3 rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
                                         MyParking
                                     </Link>
                                 </li>
                                  :
                                 <li>
                                     <NavLink to={'/'}
-                                             className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
+                                             className="block py-2 px-3 rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
                                         Parking
                                     </NavLink>
                                 </li>
@@ -73,14 +73,14 @@ const TopNavigation = () => {
                                 <li>
                                     <NavLink
                                         to={'/account'}
-                                        className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
+                                        className="block py-2 px-3 rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
                                     >Account</NavLink>
                                 </li>
                                 :
                                 <li>
                                     <NavLink
                                         to={'/login'}
-                                        className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
+                                        className="block py-2 px-3  rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
                                     >Login</NavLink>
                                 </li>
                             }
