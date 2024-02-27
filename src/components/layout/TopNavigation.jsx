08cf663cb095316de6def89566db3cf14417e0c1
jake13sa <lasusaxiom@gmail.com>
2024-02-27 14:14:37 +0800

    new ui color palette

--- src/components/layout/TopNavigation.jsx
+++ src/components/layout/TopNavigation.jsx
@@ -28,14 +28,14 @@ const TopNavigation = () => {
 
     return(
         <>
-            <nav className="border-gray-200 bg-[#0f0E17]">
+            <nav className="border-gray-200 bg-VO-Secondary">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span
-                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PFASt</span>
+                            className="self-center text-2xl font-semibold whitespace-nowrap text-VO-Tertiary">PFASt</span>
                     </a>
                     <button data-collapse-toggle="navbar-default" type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-VO-Tertiary rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-VO-Tertiary hover:bg-VO-Tertiary hover:text-VO-Secondary"
                             aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -45,19 +45,19 @@ const TopNavigation = () => {
                         </svg>
                     </button>
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-[#0f0e17] md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-[#0f0e17] dark:bg-[#0f0e17] md:dark:bg-[#0f0e17] dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
 
                             {isParkingOwner ?
                                 <li>
                                     <Link to={'/myparking'}
-                                          className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
+                                          className="block py-2 px-3 text-VO-Tertiary rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
                                         MyParking
                                     </Link>
                                 </li>
                                  :
                                 <li>
                                     <NavLink to={'/'}
-                                             className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0">
+                                             className="block py-2 px-3 text-VO-Tertiary rounded md:border-0 md:hover:text-white md:p-0">
                                         Parking
                                     </NavLink>
                                 </li>
@@ -66,14 +66,14 @@ const TopNavigation = () => {
                                 <li>
                                     <NavLink
                                         to={'/account'}
-                                        className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
+                                        className="block py-2 px-3 text-VO-Tertiary rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
                                     >Account</NavLink>
                                 </li>
                                 :
                                 <li>
                                     <NavLink
                                         to={'/login'}
-                                        className="block py-2 px-3 text-[#fffffe] rounded md:border-0 md:hover:text-[#ffd803] md:p-0"
+                                        className="block py-2 px-3 text-VO-Tertiary rounded md:border-0 md:hover:text-white md:p-0"
                                     >Login</NavLink>
                                 </li>
                             }
