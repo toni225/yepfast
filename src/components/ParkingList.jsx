e915e6bb67cc31b7f54b5243f655a6a4b1b08409
D3VASTAT3 <moblieevils@gmail.com>
2024-02-28 16:00:21 +0800

    replaced image with svg on the parking list

--- src/components/ParkingList.jsx
+++ src/components/ParkingList.jsx
@@ -38,7 +38,10 @@ const ParkingList = () => {
                 <div className="mt-10 flex justify-center -mb-10">
                     <div className="flex items-center">
                         <button onClick={()=>navigate('/parking')}>
-                            <img src={gps} className="object-contain h-20 w-20 mx-auto transition-transform duration-300 transform hover:scale-110" alt=""></img>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#262341" class="object-contain h-20 w-20 mx-auto transition-transform duration-300 transform hover:scale-110">
+  <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+</svg>
+                            {/* <img src={gps} className="object-contain h-20 w-20 mx-auto transition-transform duration-300 transform hover:scale-110" alt=""></img> */}
                             <h1 className="text-slate-800 text-2xl text-center font-sans font-bold mb-8">Explore Parkings</h1>
                         </button>
                     </div>
