6aea26af236f19f835f9d5894c7ad113cdc32b5f
jake13sa <lasusaxiom@gmail.com>
2024-03-05 14:23:41 +0800

    fix bug where displays no address

--- src/components/map/ReverseGeocoding.jsx
+++ src/components/map/ReverseGeocoding.jsx
@@ -7,16 +7,21 @@ const ReverseGeocoding = (props) => {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Latitude},${Longitude}&result_type=street_address&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
 
     const [address, setAddress] = useState([]);
-
-    const reverse = () => {
-        
-    }
+    const [responseStatus, setResponseStatus] = useState("");
 
     useEffect( () => {
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            setAddress(data.results[0].formatted_address);
+            if(data.status === "OK") {
+                setAddress(data.results[0].formatted_address);
+            } else if(data.status === "ZERO_RESULTS") {         // check if 0 result 
+                setAddress(data.plus_code.compound_code);       // then get compound_code address
+            } else if(data.status === "INVALID_REQUESTS") {
+                console.warn("the query (address, components or latlng) is missing.");
+            } else if(data.status === "UNKOWN_ERROR") {
+                console.warn("the request could not be processed due to a server error. The request may succeed if you try again.");
+            }
         })
         .catch(err => console.warn(err.message))
     })
