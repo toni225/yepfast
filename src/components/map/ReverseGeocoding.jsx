d246c0a66273c1b49ce939c57551fd55ae88b487
toni225 <73512652+toni225@users.noreply.github.com>
2024-03-06 22:53:55 +0800

    Update ReverseGeocoding.jsx

--- src/components/map/ReverseGeocoding.jsx
+++ src/components/map/ReverseGeocoding.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const ReverseGeocoding = (props) => {
     const Longitude = props.long;
     const Latitude = props.lat;
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Latitude},${Longitude}&result_type=street_address&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+    // const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Latitude},${Longitude}&result_type=street_address&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
 
     const [address, setAddress] = useState([]);
     const [responseStatus, setResponseStatus] = useState("");
