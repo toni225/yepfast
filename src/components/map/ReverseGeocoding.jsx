e48704f594743606a0a1666fe84b165733614519
jake13sa <lasusaxiom@gmail.com>
2024-03-07 13:13:30 +0800

    changed googlemaps to openstreet map

--- src/components/map/ReverseGeocoding.jsx
+++ src/components/map/ReverseGeocoding.jsx
@@ -5,6 +5,7 @@ const ReverseGeocoding = (props) => {
     const Longitude = props.long;
     const Latitude = props.lat;
     // const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Latitude},${Longitude}&result_type=street_address&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${Latitude}&lon=${Longitude}&format=json`
 
     const [address, setAddress] = useState([]);
     const [responseStatus, setResponseStatus] = useState("");
@@ -13,15 +14,7 @@ const ReverseGeocoding = (props) => {
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            if(data.status === "OK") {
-                setAddress(data.results[0].formatted_address);
-            } else if(data.status === "ZERO_RESULTS") {         // check if 0 result 
-                setAddress(data.plus_code.compound_code);       // then get compound_code address
-            } else if(data.status === "INVALID_REQUESTS") {
-                console.warn("the query (address, components or latlng) is missing.");
-            } else if(data.status === "UNKOWN_ERROR") {
-                console.warn("the request could not be processed due to a server error. The request may succeed if you try again.");
-            }
+            setAddress(data.display_name)
         })
         .catch(err => console.warn(err.message))
     })
