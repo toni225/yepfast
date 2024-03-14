06e40b9041d7618b21624e29b35ead58699bedfa
D3VASTAT3 <moblieevils@gmail.com>
2024-03-14 21:36:48 +0800

    first save

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,85 +1,68 @@
-import * as userServices from '../services/user.service'
-
-import {Box, Button} from "@mui/material";
-
-import {useEffect, useState} from "react";
+import { Box, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as userServices from "../services/user.service";
+import Layout from "./layout/Layout";
 
 const AdminPage = () => {
-    const [reports,setReports] = useState([])
+    const navigate = useNavigate();
+    const [reports, setReports] = useState([]);
 
-    const fetchUser = async () => {
-        try{
-            const {data: {users}} = await userServices.getAllReports()
-            setReports(users)
-            // console.log(users)
-        }catch (e) {
-            console.log(e)
+    // Function to fetch reports from the server
+    const fetchReports = async () => {
+        try {
+            const { data: { users } } = await userServices.getAllReports();
+            setReports(users);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-    const banParking = async (reportID,parkingID) => {
-        try{
-            // console.log(`reportid: ${reportID}, parkingid: ${parkingID}`)
-            if(window.confirm("Ban now?")){
-                userServices.banParking(reportID).then(res=>{
-                    console.log(res)
-                    userServices.removeParking(parkingID).then(res=>{
-                        console.log(res)
-                        window.location.reload()
-                    })
-                })
+    // Function to ban parking based on report ID and parking ID
+    const banParking = async (reportID, parkingID) => {
+        try {
+            if (window.confirm("Ban now?")) {
+                await userServices.banParking(reportID);
+                await userServices.removeParking(parkingID);
+                window.location.reload(); // This might not be the best way to refresh the page
             }
-        }catch (e){
-            console.log(e)
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-    useEffect(()=>{
-        fetchUser()
-        // console.log(reports)
-    },[])
+    useEffect(() => {
+        fetchReports();
+    }, []);
 
-  return (
-      <div className={'mt-20 flex justify-center'}>
-          <div className={'flex justify-center'}>
-                <Box>
-                    Reports
-                    <table className={'table-auto border-collapse'}>
-                        <thead className={'uppercase'}>
-                            <tr>
-                                <th scope={'col'} className="px-10 py-3">ID</th>
-                                <th scope={'col'} className="px-10 py-3">Parking Name</th>
-                                <th scope={'col'} className="px-10 py-3">Reports</th>
-                                <th scope={'col'} className="px-16 py-3">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {reports.map(report=> {
-                                return (
-                                    <tr className={'hover:bg-gray-300 border-b border-slate-400'} key={report.ReportID}>
-                            {/*        <tr className={'border-b '} onClick={e=>console.log(e.target.rowIndex)}>*/}
-                                        <th scope={'row'}>
-                                            {report.ParkingID}
-                                            {/*1*/}
-                                        </th>
-                                        <td className={'text-center'}>{report.ParkingDetails.ParkingName}</td>
-                                        {/*<td>etet</td>*/}
-                                        {/*<td>etet</td>*/}
-                                        {/*<td>etet</td>*/}
-                                        <td className={'text-center'}>{report.count}</td>
-                                        <td className={'flex gap-2 justify-center'}>
-                                            <Button variant={'outlined'} onClick={()=>console.log(`warning ${report.ParkingID}`)}>Warning</Button>
-                                            <Button variant={'contained'} onClick={()=>banParking(report.ReportID,report.ParkingID)}>Ban</Button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </Box>
-          </div>
-      </div>
-  )
-}
+    return (
+        <Layout>
+            <div id="entire-container">
+                <ul className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-6 p-10">
+                    {/* Mapping through each report to display */}
+                    {reports.map(report => (
+                        <li key={report.ReportID} className="bg-Admin-Secondary text-gray-800 w-[300px] h-auto text-left mx-auto rounded-md relative transition-transform duration-300 transform hover:scale-110">
+                            <div className="px-3 py-2">
+                                {/* Displaying Parking Name */}
+                                <div className="py-2 overflow-ellipsis truncate text-2xl">{report.ParkingDetails.ParkingName}</div>
+                                {/* Displaying Parking ID */}
+                                <div className="py-2 text-xs">{`ID: ${report.ParkingID}`}</div>
+                                {/* Displaying number of reports */}
+                                <div className="py-2 text-xs">{`Reports: ${report.count}`}</div>
+                                {/*Displaying description*/}
+                                <div className="w-[217px] h-[108px] text-white text-xs font-normal font-['Poppins']">“The parking owner didn’t follow his set price on the site. A friend of mine also complained that he also got forced to pay otherwise the parking owner threatened to sue him for trespassing.”</div>
+                                {/* Action buttons */}
+                                <div className="flex justify-between mt-3">
+                                    <Button variant="outlined" sx={{ bgcolor: '#F9D94A', color: 'black' }} onClick={() => console.log(`warning ${report.ParkingID}`)}>Warning</Button>
+                                    <Button variant="contained" sx={{ bgcolor: '#F94A4A', color: 'black' }}  onClick={() => banParking(report.ReportID, report.ParkingID)}>Ban</Button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </Layout>
+    );
+};
 
-export default AdminPage
+export default AdminPage;
