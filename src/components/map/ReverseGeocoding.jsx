5214e42239c742d9a69c21d93a4b7f2558b2adcb
jake13sa <lasusaxiom@gmail.com>
2024-03-21 13:03:35 +0800

    remove api; changed to localhost

--- src/components/map/ReverseGeocoding.jsx
+++ src/components/map/ReverseGeocoding.jsx
@@ -5,7 +5,8 @@ const ReverseGeocoding = (props) => {
     const Longitude = props.long;
     const Latitude = props.lat;
     // const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Latitude},${Longitude}&result_type=street_address&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
-    const url = `https://nominatim.openstreetmap.org/reverse?lat=${Latitude}&lon=${Longitude}&format=json`
+    // const url = `https://nominatim.openstreetmap.org/reverse?lat=${Latitude}&lon=${Longitude}&format=json`
+    const url = "localhost"
 
     const [address, setAddress] = useState([]);
     const [responseStatus, setResponseStatus] = useState("");
