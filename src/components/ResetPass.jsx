8be8424a160e6da520bd600f7b6686a08f77f8a8
D3VASTAT3 <moblieevils@gmail.com>
2024-03-30 16:52:40 +0800

    updated forget pass design, integrated toast

--- src/components/ResetPass.jsx
+++ src/components/ResetPass.jsx
@@ -1,11 +1,20 @@
 import * as userService from '../services/user.service'
 import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 
 const ResetPass = () => {
     const [email, setEmail] = useState('')
+    const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        if (!email.trim()) {
+            // If email is empty, handle the case here
+            toast.warning("Please enter a valid email address.")
+            return;
+        }
 
         try {
             const res = userService.resetPass({ email })
@@ -30,9 +39,16 @@ const ResetPass = () => {
                         placeholder={' '} onChange={e => setEmail(e.target.value)} />
                     <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-yellow-500 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-yellow-600 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-[#ff8906] peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-yellow-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-yellow-600 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white"> Email </label>
                 </div>
-                <button
-                    className="my-5 inline-block mx-52 px-3 bg-VO-Tertiary select-none rounded-lg py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    type={'submit'}>Next</button>
+                <div className="mx-12 flex justify-between mt-5">
+                    <button
+                        className="my-5 w-20 inline-block px-3 bg-VO-Tertiary select-none rounded-lg py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        onClick={()=>navigate(-1)}
+                        type={'button'}>Cancel</button>
+
+                    <button
+                        className="my-5 w-20 inline-block px-3 bg-VO-Tertiary select-none rounded-lg py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type={'submit'}>Next</button>
+                </div>
             </form>
         </div>
     )
