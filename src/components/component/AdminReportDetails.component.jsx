b8aac0b5a31bdeb6b507a2f85d881abf207aac4f
D3VASTAT3 <moblieevils@gmail.com>
2024-04-05 15:22:18 +0800

    commit#2

--- src/components/component/AdminReportDetails.component.jsx
+++ src/components/component/AdminReportDetails.component.jsx
@@ -67,25 +67,27 @@ const AdminReportDetailsComponent = ({ ParkingID }) => {
             >
                 <Box sx={style}>
                     <h2 id="child-modal-title">Parking Report Details</h2>
-                    <div style={{ maxHeight: '60vh', overflowY: 'auto' }}>
-                        {reports.map((report, index) => (
-                            <div key={report.ReportID} className="bg-white shadow-md rounded-md p-6 mb-4">
+                    <div className='bg-gray-800' style={{ maxHeight: '60vh', overflowY: 'auto' }}>
+                        {reports
+                        .sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
+                        .map((report, index) => (
+                            <div key={report.ReportID} className="font-['Inconsolata'] text-white bg-gray-800 p-6 mb-4">
                                 <h3 className="text-lg font-semibold mb-2">Report No. {index + 1}</h3>
                                 <div className="space-y-2">
                                     <p><strong>Reported By:</strong> {report.username}</p>
-                                    <p><strong>Reason:</strong> {report.body}</p>
                                     <p><strong>Time:</strong> {new Date(report.created_at).toLocaleString('en-US', {
                                         dateStyle: 'long',
                                         timeStyle: 'short'
                                     })}</p>
+                                    <p><strong>Reason:</strong> {report.body}</p>
                                 </div>
                             </div>
                         ))}
                     </div>
-                    <div className="flex justify-end">
-                        <Button variant="outlined" onClick={handleClose} style={{ marginRight: '10px' }}>Close</Button>
-                        <Button variant="outlined" onClick={() => console.log(`warning ${reports[0]?.ParkingID}`)} style={{ marginRight: '10px' }}>Warning</Button>
-                        <Button variant="outlined" onClick={() => banParking(reports[0]?.ReportID, reports[0]?.ParkingID)}>Ban</Button>
+                    <div className="pt-6 flex justify-end">
+                        <Button variant="contained" onClick={handleClose} style={{ marginRight: '10px' }}>Close</Button>
+                        <Button variant="contained" color="warning" onClick={() => console.log(`warning ${reports[0]?.ParkingID}`)} style={{ marginRight: '10px' }}>Warning</Button>
+                        <Button variant="contained" color="error" onClick={() => banParking(reports[0]?.ReportID, reports[0]?.ParkingID)}>Ban</Button>
                     </div>
                 </Box>
             </Modal>
